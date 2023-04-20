3a0c0a385610ba9e4c6b1a8d13037a8c18183116
Jagannathan CS <jagannathan606@gmail.com>
2023-04-20 23:42:04 +0530

    Css updated

--- script.js
+++ script.js
@@ -71,7 +71,6 @@ darkModeIcone.addEventListener('click', toggleDarkMode);
 ///////scroll reveal /////////////////////////////////////////////////////
 
 ScrollReveal({
-    reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200,
